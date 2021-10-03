33370dd4b366d61b21924404a19a7ab4452b9684
Nikita <64281268+AlinaRinn@users.noreply.github.com>
2021-10-04 01:30:48 +0300

    Create footer.php

---
+++ layouts/footer.php
@@ -0,0 +1,13 @@
+</main>
+<footer>
+    <p>
+        Tofu inc. 2021 	 &copy;  All right received
+        <a href="https://jigsaw.w3.org/css-validator/check/referer">
+            <img style="position: absolute;margin-bottom: 50%;margin-left: 39%;border:0;width:88px;height:31px;"
+                 src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+                 alt="Правильный CSS!" />
+        </a>
+    </p>
+</footer>
+</body>
+</html>
