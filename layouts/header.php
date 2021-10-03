9ab8707a0f378f5af262a8f989ce7d327c7b5089
Nikita <64281268+AlinaRinn@users.noreply.github.com>
2021-10-04 01:30:55 +0300

    Add files via upload

---
+++ layouts/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Tofu Driver</title>
+        <link href="/css/reset.css" rel="stylesheet" type="text/css"/> <!-- Примечание автора ;) : Поднятие на директорию вверх "../ " не нужно, тк header.php склеивается с index.php и итоговые пути меняются-->
+        <link href="/css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="/images/tofu_full_opacity.ico" rel="shortcut icon"/>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <div id="name">
+                <div id="namelogo">
+                    <a href="/index.php"><img src="/images/logonew.jpg" alt="Logo err"></a>
+                </div>
+                <a href="/index.php"><div id="nametxt">
+                    TOFU DRIVER
+                    </div></a>
+            </div>
+
+            <nav>
+                <a href="/index.php">Главная</a>
+                <a href="/catalog.php">Каталог</a>
+                <a href="/request.php">Подать заявку</a>
+                <a href="/partners.php">Партнеры</a>
+                <a href="/about.php">О нас</a>
+            </nav>
+        </header>
+<main>
